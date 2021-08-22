e79e8c3fecee6dbcedb1c1f31a7ed9061c7c79d6
Kaveh Nematipour <knematipour@me.com>
2021-08-22 12:22:35 +0300

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Adder/adder.h"
+
+using namespace std; 
+
+int main(){
+    cout << "Hello World!" << endl;
+    cout << "Using adder function: " << add(51.1f,72.4f) << endl; 
+    return 0; 
+}
