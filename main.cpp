8d527014d35234a592518ca3dbaa35e121a2522c
Kaveh Nematipour <knematipour@me.com>
2021-08-23 15:52:24 +0300

    Testing the Boost Thread Lib

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include "Adder/adder.h"
+#include <boost/lambda/lambda.hpp>
+#include <boost/thread.hpp>
 
 using namespace std; 
 
+void bankAgent(){
+    for(int i = 0;i<10;i++)
+        cout << i << endl; 
+}
+
 int main(){
+    using namespace boost::lambda;
+    
+    boost::thread thread1(bankAgent); 
+    //thread1.join(); 
+
+
     cout << "Hello World!" << endl;
     cout << "Using adder function: " << add(51.1f,72.4f) << endl; 
     return 0; 
